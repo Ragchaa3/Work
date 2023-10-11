0e5497f804f263920191af59baac54a1377ef9e0
Ragchaa3 <146328063+Ragchaa3@users.noreply.github.com>
2023-10-11 09:40:45 +0800

    Add files via upload

--- poll.php
+++ poll.php
@@ -2,36 +2,54 @@
 <html>
 <head>
     <title>Санал асуулга</title>
+    <!-- Bootstrap сангийн холбоосуудыг оруулах -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-<?php
-    // Санал өгсөн эсэхийг шалгах
-    if (isset($_COOKIE['last_poll_time'])) {
-        $last_poll_time = $_COOKIE['last_poll_time'];
-        $current_time = time();
-        $time_difference = $current_time - $last_poll_time;
-
-        if ($time_difference < 180) {
-            echo "Дахин санал өгөх боломжгүй";
-        }
-    }
-
+<div class="container mt-4">
+    <?php
     // poll.txt файлыг уншиж, санал асуулгыг үзүүлэх
     $lines = file("poll.txt", FILE_IGNORE_NEW_LINES);
-    
-    // Санал асуулгыг үзүүлэх
     echo "<h1>".$lines[0]."</h1>";
-    echo "<form method='post' action='submit_poll.php'>";
-    
-    // Хариултуудыг үзүүлэх
+    echo "<form method='post'>";
+
     for ($i = 1; $i <= 3; $i++) {
-        echo "<input type='radio' name='answer' value='$i'>".$lines[$i]."<br>";
+        echo "<div class='form-check'>";
+        echo "<input type='radio' class='form-check-input' name='answer' value='$i' id='answer$i'>";
+        echo "<label class='form-check-label' for='answer$i'>".$lines[$i]."</label>";
+        echo "</div>";
     }
-    
-    // Санал өгсөн тоог файлын харгалзах талбарт хадгалах
-    echo "<input type='submit' value='Санал өгөх'>";
-    
+    echo "<input type='submit' name='submit' value='Санал өгөх' class='btn btn-primary'>";
     echo "</form>";
     ?>
+    <?php
+
+    if (isset($_POST['answer']) && isset($_POST['submit'])) {
+        $selectedAnswer = $_POST['answer'];
+        $pollName = 'poll_form';
+        if (!isset($_COOKIE[$pollName])) {
+            setcookie($pollName, 'submitted', time() + 180); // 3 минут хугацаанд нэг удаа
+            echo "<div class='alert alert-success'>Санал амжилттай илгээгдлээ</div>";
+            // Саналыг файлд хадгалах
+            $file = fopen("poll_results.txt", "a");
+            if ($file) {
+                fwrite($file, $selectedAnswer . "\n");
+                fclose($file);
+            }
+        } else {
+            echo "<div class='alert alert-danger'>3 минутын дотор дахин санал өгөх боломжгүй</div>";
+        }
+    }
+    ?>
+    <?php
+    // "poll_results.txt" файлыг унших
+    $pollResults = file("poll_results.txt", FILE_IGNORE_NEW_LINES);
+    $totalVotes = count($pollResults);
+    echo "<p class='mt-3'>Нийт саналын тоо: $totalVotes</p>";
+    ?>
+</div>
 </body>
 </html>
